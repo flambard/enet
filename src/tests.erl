8d3bb9c2cd91cea8fe17f8ec0390e32c180497ee
Markus Flambard <markus@flambard.se>
2017-08-15 10:47:04 +0200

    Stop host controllers after running the connect test

--- src/tests.erl
+++ src/tests.erl
@@ -6,6 +6,9 @@
 dual_connect() ->
     {ok, H1} = host_controller:start_link(5001, [{peer_limit, 8}]),
     {ok, H2} = host_controller:start_link(5002, [{peer_limit, 8}]),
-    peer_controller:local_connect(H1, "127.0.0.1", 5002),
-    peer_controller:local_connect(H2, "127.0.0.1", 5001),
+    {ok, _Pid1} = peer_controller:local_connect(H1, "127.0.0.1", 5002),
+    {ok, _Pid2} = peer_controller:local_connect(H2, "127.0.0.1", 5001),
+    receive after 5000 -> ok end,
+    ok = host_controller:stop(H1),
+    ok = host_controller:stop(H2),
     ok.
