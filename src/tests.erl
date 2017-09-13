83a0253e68210986a5b663307744ed0500dbe01b
Markus Flambard <markus@flambard.se>
2017-09-13 08:47:39 +0200

    Use Eunit properly for connect/disconnect tests.

--- src/tests.erl
+++ src/tests.erl
@@ -1,14 +1,38 @@
 -module(tests).
 
--compile(export_all).
+-include_lib("eunit/include/eunit.hrl").
 
 
-dual_connect() ->
-    {ok, H1} = host_controller:start_link(5001, [{peer_limit, 8}]),
-    {ok, H2} = host_controller:start_link(5002, [{peer_limit, 8}]),
-    {ok, _Pid1} = peer_controller:local_connect(H1, "127.0.0.1", 5002),
-    {ok, _Pid2} = peer_controller:local_connect(H2, "127.0.0.1", 5001),
-    receive after 5000 -> ok end,
-    ok = host_controller:stop(H1),
-    ok = host_controller:stop(H2),
+local_disconnect_test() ->
+    {ok, L} = host_controller:start_link(5001, [{peer_limit, 8}]),
+    {ok, R} = host_controller:start_link(5002, [{peer_limit, 8}]),
+    {ok, Peer} = peer_controller:local_connect(L, "127.0.0.1", 5002),
+    Ref = erlang:monitor(process, Peer),
+    receive after 500 -> continue end,
+    ok = peer_controller:disconnect(Peer),
+    receive
+        {'DOWN', Ref, process, Peer, normal} ->
+            ok
+    after 1000 ->
+            exit(peer_did_not_exit)
+    end,
+    ok = host_controller:stop(L),
+    ok = host_controller:stop(R),
+    ok.
+
+remote_disconnect_test() ->
+    {ok, L} = host_controller:start_link(5001, [{peer_limit, 8}]),
+    {ok, R} = host_controller:start_link(5002, [{peer_limit, 8}]),
+    {ok, Peer} = peer_controller:local_connect(R, "127.0.0.1", 5002),
+    Ref = erlang:monitor(process, Peer),
+    receive after 500 -> continue end,
+    ok = peer_controller:disconnect(Peer),
+    receive
+        {'DOWN', Ref, process, Peer, normal} ->
+            ok
+    after 1000 ->
+            exit(peer_did_not_exit)
+    end,
+    ok = host_controller:stop(L),
+    ok = host_controller:stop(R),
     ok.
