4d369fe02200bd473a899523d0dd2081eef1cd90
Markus Flambard <markus@flambard.se>
2017-09-19 19:37:07 +0200

    Added peer limit tests.

--- src/tests.erl
+++ src/tests.erl
@@ -3,6 +3,32 @@
 -include_lib("eunit/include/eunit.hrl").
 
 
+local_zero_peer_limit_test() ->
+    LPort = 5001,
+    {ok, LHC} = enet:start_host(LPort, [{peer_limit, 0}]),
+    RPort = 5002,
+    {ok, _RHC} = enet:start_host(RPort, [{peer_limit, 1}]),
+    {error, reached_peer_limit} = enet:connect_peer(LHC, "127.0.0.1", RPort),
+    ok = enet:stop_host(LPort),
+    ok = enet:stop_host(RPort).
+
+remote_zero_peer_limit_test() ->
+    LPort = 5001,
+    {ok, LHC} = enet:start_host(LPort, [{peer_limit, 1}]),
+    RPort = 5002,
+    {ok, _RHC} = enet:start_host(RPort, [{peer_limit, 0}]),
+    {ok, Peer} = enet:connect_peer(LHC, "127.0.0.1", RPort),
+    receive
+        {enet, connect, local, Peer} ->
+            exit(peer_could_connect_despite_peer_limit_reached);
+        {enet, connect, remote, _RemotePeer} ->
+            exit(remote_peer_started_despite_peer_limit_reached)
+    after 200 -> %% How long time is enough? (This is ugly)
+            ok
+    end,
+    ok = enet:stop_host(LPort),
+    ok = enet:stop_host(RPort).
+
 async_connect_and_local_disconnect_test() ->
     LPort = 5001,
     {ok, LHC} = enet:start_host(LPort, [{peer_limit, 8}]),
