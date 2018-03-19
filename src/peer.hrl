c011e4e46a87ec18e4582be6cfb703e6eddb13fb
Markus Flambard <markus@flambard.se>
2018-03-19 21:33:39 +0100

    Removed incoming_session_id and outgoing_session_id from #peer record.

--- src/peer.hrl
+++ src/peer.hrl
@@ -1,9 +1,8 @@
 -record(peer,
-        { id
-        , pid
-        , remote_id
-        , ip
-        , port
-        , incoming_session_id = 0
-        , outgoing_session_id = 0
+        {
+          id,
+          pid,
+          remote_id,
+          ip,
+          port
         }).
