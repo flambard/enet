931df2ca4f5e676b376ff602f5ff37197c5fcccb
Markus Flambard <markus@flambard.se>
2018-03-21 21:35:56 +0100

    Added typespecs on API functions in enet.erl.

--- src/enet.erl
+++ src/enet.erl
@@ -12,6 +12,12 @@
         ]).
 
 
+-type port_number() :: 0..65535.
+
+
+-spec start_host(Port :: port_number(), Options :: [{atom(), term()}]) ->
+                        {ok, pid()} | {error, atom()}.
+
 start_host(Port, Options) ->
     {ok, HostSup} = enet_sup:start_host_supervisor(Port),
     {ok, PeerSup} = host_sup:start_peer_supervisor(HostSup),
@@ -22,25 +28,48 @@ start_host(Port, Options) ->
             {error, Reason}
     end.
 
+
+-spec stop_host(Port :: port_number()) -> ok.
+
 stop_host(Port) ->
     enet_sup:stop_host_supervisor(Port).
 
 
+-spec connect_peer(Host :: pid(), IP :: string(), Port :: port_number(),
+                   ChannelCount :: pos_integer()) ->
+                          {ok, pid()} | {error, atom()}.
+
 connect_peer(Host, IP, Port, ChannelCount) ->
     host_controller:connect(Host, IP, Port, ChannelCount).
 
+
+-spec sync_connect_peer(Host :: pid(), IP :: string(), Port :: port_number(),
+                        ChannelCount :: pos_integer()) ->
+                               {ok, pid()} | {error, atom()}.
+
 sync_connect_peer(Host, IP, Port, ChannelCount) ->
     host_controller:sync_connect(Host, IP, Port, ChannelCount).
+
+
+-spec disconnect_peer(Peer :: pid()) -> ok.
 
 disconnect_peer(Peer) ->
     peer_controller:disconnect(Peer).
 
 
+-spec send_unsequenced(Channel :: pid(), Data :: iolist()) -> ok.
+
 send_unsequenced(Channel, Data) ->
     channel:send_unsequenced(Channel, Data).
 
+
+-spec send_unreliable(Channel :: pid(), Data :: iolist()) -> ok.
+
 send_unreliable(Channel, Data) ->
     channel:send_unreliable(Channel, Data).
+
+
+-spec send_reliable(Channel :: pid(), Data :: iolist()) -> ok.
 
 send_reliable(Channel, Data) ->
     channel:send_reliable(Channel, Data).
