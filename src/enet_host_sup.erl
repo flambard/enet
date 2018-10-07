eeddc06e064b704e9784b2c1e770ff9e3e27ae5e
Markus Flambard <markus@flambard.se>
2018-10-07 20:07:37 +0200

    The peer supervisor should stop before the host
    To give the host some time to send out the unsequenced DISCONNECT
    commands, we shut down the peer supervisor and peers first while the
    host is still running.

--- src/enet_host_sup.erl
+++ src/enet_host_sup.erl
@@ -28,18 +28,6 @@ init([Owner, Port, Options]) ->
       intensity => 0, %% <- Zero tolerance for crashes
       period => 1
      },
-    PeerSup = #{
-                id => enet_peer_sup,
-                start => {
-                          enet_peer_sup,
-                          start_link,
-                          [Port]
-                         },
-                restart => permanent,
-                shutdown => infinity,
-                type => supervisor,
-                modules => [enet_peer_sup]
-               },
     Host = #{
              id => host,
              start => {
@@ -52,7 +40,19 @@ init([Owner, Port, Options]) ->
              type => worker,
              modules => [enet_host]
             },
-    {ok, {SupFlags, [PeerSup, Host]}}.
+    PeerSup = #{
+                id => enet_peer_sup,
+                start => {
+                          enet_peer_sup,
+                          start_link,
+                          [Port]
+                         },
+                restart => permanent,
+                shutdown => infinity,
+                type => supervisor,
+                modules => [enet_peer_sup]
+               },
+    {ok, {SupFlags, [Host, PeerSup]}}.
 
 
 %%%===================================================================
